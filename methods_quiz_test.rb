23d01b026f75ac44829d3b4d158029e6a1fab9fc
Colter Giem <giemcl@s.dcsdk12.org>
2015-09-02 09:46:20 -0600

    Tests for has_teen?

--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -11,6 +11,11 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
-	# TODO - write tests here
+	def test_has_teen
+		assert_equal true, @m.has_teen?(1,19,12)
+		assert_equal true, @m.has_teen?(19,1,12)
+		assert_equal true, @m.has_teen?(1,12,19)
+		assert_equal false, @m.has_teen?(1,12,20)
+	end
 
 end
